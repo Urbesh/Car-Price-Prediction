7fc101efe962a793f6bbe07b02eeba662abaaa90
Urbesh Mondal <urbeshmondal@gmail.com>
2025-11-17 13:45:56 +0530

    Removed the unwanted libraries that were imported

--- Full Project/Car Prediction Project.py
+++ Full Project/Car Prediction Project.py
@@ -7,11 +7,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import cross_val_score
 
 MODEL_FILE="Model/model.pkl"
 PIPELINE_FILE="Model/pipeline.pkl"
